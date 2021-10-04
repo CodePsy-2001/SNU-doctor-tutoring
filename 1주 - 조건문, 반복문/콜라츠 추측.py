d22051a22c65aef8e2e34da51ff2420c99d00060
CodePsy-2001 <81800589+CodePsy-2001@users.noreply.github.com>
2021-10-04 18:57:12 +0900

    Update 콜라츠 추측.py

--- 1주 - 조건문, 반복문/콜라츠 추측.py
+++ 1주 - 조건문, 반복문/콜라츠 추측.py
@@ -1,3 +1,7 @@
+# 콜라츠 추측 계산기
+# 1부터 MAXnum 까지의 콜라츠 수열을 계산한다.
+# 제한조건 : print 함수는 numlist 를 출력하는 데에만 사용할 수 있다.
+
 MAXnum = 10000
 
 for i in range(1, MAXnum+1):
@@ -11,5 +15,3 @@ for i in range(1, MAXnum+1):
             num = num*3 + 1
         numlist.append(num)
     print(numlist)
-
-# 콜라츠 추측 계산기
